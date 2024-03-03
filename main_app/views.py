c67059737f8a9ea189446c491d4a60d9aff760d1
Ga1o <ga1ogramma@yandex.ru>
2024-03-04 00:47:55 +0300

    Add about page

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,7 @@ class IndexView(View):
     def get(self, request):
         return render(request, 'main_app/index.html')
 
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'main_app/about.html')
