b735c3fbdd878c6f0d32a696e2522d581905e063
Ga1o <ga1ogramma@yandex.ru>
2024-03-05 06:28:07 +0300

    Регистрация модели избранных продуктов в админке.

--- favorites_app/admin.py
+++ favorites_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import FavoriteProduct
 
-# Register your models here.
+
+@admin.register(FavoriteProduct)
+class FavoriteProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'product_id', 'data_add', 'data_remove')
+    list_display_links = ('id', 'user_id')
